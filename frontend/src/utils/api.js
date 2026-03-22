676ca433340b3b1f1d6e2ea8da9317aa490cb85b
fahadmohamed96p-sys <fahadmohamed96p@gmail.com>
2026-03-22 10:24:39 +0530

    fix: host

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://placementpro-hpzg.onrender.com/',
-})
+const BASE = (import.meta.env.VITE_API_URL || 'https://placementpro-hpzg.onrender.com').replace(/\/$/, '')
+
+const api = axios.create({ baseURL: BASE })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('pp_token')
